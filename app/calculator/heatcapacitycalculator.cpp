efd071cc173a869ef85759754d0b06a9d757b581
Kudukdk <kusukdk@gmail.com>
2022-05-07 18:40:28 +0300

    Update heatcapacitycalculator.cpp

--- app/calculator/heatcapacitycalculator.cpp
+++ app/calculator/heatcapacitycalculator.cpp
@@ -3,6 +3,15 @@
 
 HeatCapacityCalculator::HeatCapacityCalculator(DataStorage * storage) : Calculator(storage)
 {
+
+}
+void HeatCapacityCalculator::Tczh(){
+   // $("T_e") = T_b+T_b*((pow((3,14),(R_a)/(C_pms_A))-1)*(1/E);
+}     $("T_e") = $("T_b")+$("T_b")*((pow((3,14),(($("R"))/($("C_pms_A")))))-1)*($("E"));
+
+void HeatCapacityCalculator::Tras(){
+    $("T_e") = $("T_b")+$("T_b")*(1-(pow((3,14),(($("R"))/($("C_pms_G")))))*($("E"));
+
 }
 
 double HeatCapacityCalculator::calculate()
